7e9e68ce2b129aed287499aed815e9694748c47d
Mwambutsa Edgar <herLgroup@Mwambutsas-MacBook-Air.local>
2020-02-05 19:40:31 +0300

    edgarpaye

---
+++ edgarpaye.js
@@ -0,0 +1,12 @@
+
+    let salary=1000000
+    let tax=30/100
+    let taxrate=tax*salary
+    let netpay=salary-taxrate
+
+    function paye ()
+    {
+        let pay=[netpay]
+        console.log (netpay)
+    }
+    paye()
